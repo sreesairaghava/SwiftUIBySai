fa41dddbbd0bf1e58ee82c96a88e8e4d7ad66eea
Sree Sai Raghava <raghava.rym@gmail.com>
2025-04-21 19:58:28 +0530

    Initial Commit

---
+++ SwiftUIBySai/SwiftUIBySaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBySaiApp.swift
+//  SwiftUIBySai
+//
+//  Created by Sree Sai Raghava Dandu on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBySaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
